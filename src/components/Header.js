2b6e325ddfe4f7917bc606f8663012b8b5898b46
Najmul-Islam <najmulislam519@gmail.com>
2021-04-23 04:49:35 +0600

    done

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header (){
+    return(
+        <header className="navbar">
+            <h1>This is the header</h1>
+        </header>
+    )
+}
+
+export default Header
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,8 +3,7 @@ import React from 'react'
 function MainContent (){
     return(
         <main>
-            <h2>This is MainContent component</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum maxime ad perspiciatis! Magnam quos dicta natus reiciendis non mollitia obcaecati eveniet! Saepe perspiciatis impedit unde voluptatem, laboriosam dignissimos fuga maxime.</p>
+            <h2>This is MainContent</h2>
         </main>
     )
 }

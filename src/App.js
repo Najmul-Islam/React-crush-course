e15dbe799ef035494f0b8b9e4e762842e7d2ad77
Najmul-Islam <najmulislam519@gmail.com>
2021-04-23 01:14:22 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react"
+import Navbar from './components/Navbar'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 
 function App(){
   return(
     <div>
-      Hello world
+      <Navbar />
+      <MainContent />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/MainContent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MainContent (){
+    return(
+        <main>
+            <h2>This is MainContent component</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum maxime ad perspiciatis! Magnam quos dicta natus reiciendis non mollitia obcaecati eveniet! Saepe perspiciatis impedit unde voluptatem, laboriosam dignissimos fuga maxime.</p>
+        </main>
+    )
+}
+
+export default MainContent

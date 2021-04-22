47d1486892d459b92e3365308f87e63b9f10e02e
Najmul-Islam <najmulislam519@gmail.com>
2021-04-22 15:39:13 +0600

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import App from "./App"
+function MyInfo(){
+  const heading={
+    color:'red',
+    fontSize:'24',
+    textAlign: 'center'
+  }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+  const pargraph = {
+    textAlign: 'center',
+    fontSize: '18'
+  }
+
+  const unorderList = {
+    fontSize: '20'
+  } 
+  return(
+    <div>
+      <h1 style={heading}>Najmul Islam</h1>
+      <p style={pargraph}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro alias aut nemo cumque ea voluptatem asperiores magnam tempora delectus exercitationem?</p>
+      <ul style={unorderList}>
+        <li>Shelet</li>
+        <li>Kholna</li>
+        <li>Rangamati</li>
+      </ul>
+    </div>
+  )
+}
+
+ReactDOM.render(<MyInfo />, document.getElementById('root'));

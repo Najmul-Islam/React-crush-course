09f159c037f4847ddd91842bd2a1ba36aa2a3b4b
Najmul-Islam <najmulislam519@gmail.com>
2021-04-24 01:47:47 +0600

    done inline styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React from "react"
 
 function App(){
+  const date = new Date()
+  const hours = date.getHours()
+  let timeOfDay
+  const styles = {
+    fontSize: 30
+  }
+  
+  if(hours < 12){
+    timeOfDay = "Morning"
+    styles.color= "green"
+  }else if(hours >= 12 && hours < 17){
+    timeOfDay = "Afternoon"
+    styles.color="red"
+  }else{
+    timeOfDay = "night"
+    styles.color = "pink"
+  }
+
+  // const styles = {
+  //   color: "red", 
+  //   backgroundColor: "pink",
+  //   fontSize: 24
+  // }
+
   return(
-    <div>
-      Hello world
+    <div> 
+    <h1 style={styles}>Good {timeOfDay}</h1>
     </div>
   )
 }

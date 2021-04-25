3ea717a68ac43d23b2a4f842d46954465e99fedb
Najmul-Islam <najmulislam519@gmail.com>
2021-04-25 12:23:13 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-class App extends React.Component {
+class App extends Component {
     constructor(){
         super()
         this.state = {
-            answer: "Yes"
+            name: 'Najmul',
+            age: 23
         }
     }
 
     render(){
-        return (
+        return(
             <div>
-                <h1>Is state importent to know? {this.state.answer}</h1>
+                <h1>{this.state.name}</h1>
+                <h3>{this.state.age} years old</h3>
             </div>
         )
     }

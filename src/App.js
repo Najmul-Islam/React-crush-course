ee9cdd7a814efed267017ad19f7224fb8d221238
Najmul-Islam <najmulislam519@gmail.com>
2021-04-25 23:40:17 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function App () {
-    return(
-        <div>
-            
-        </div>
-    )
+class App extends Component{
+    constructor(){
+        super()
+        this.state = {
+            count: 0
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick (){
+      this.setState((prevState)=>{
+        return{
+            count: prevState.count + 1
+        }
+      })
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>{this.state.count}</h1>
+                <button onClick={this.handleClick}>Change!</button>
+            </div>
+        )
+    }
 }
 
 export default App

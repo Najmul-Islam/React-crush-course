44ad32fb3f4b0acaa6f22ff0445f07c306d33399
Najmul-Islam <najmulislam519@gmail.com>
2021-04-26 00:11:18 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function App () {
-    return(
-        <div>
-            
-        </div>
-    )
+class App extends Component {
+    constructor (){
+        super()
+        this.state = {}
+    }
+
+    componentDidMount() {
+        // GET the dat I need to correctly display
+    }
+
+    componentWillReceiveProps(nextProps){
+        if(nextProps.whatever !== this.props.whatever) {
+            // do something important here
+        }
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        // return true if want it to update
+        // return false if not
+    }
+
+    componentWillUnmount(){
+        // teardown or cleanup your code before your conmponent disappears
+        // (E.g. remove event listeners)
+    }
+    
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
 }
 
 export default App

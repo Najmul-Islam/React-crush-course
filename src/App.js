7abff7d383dd7fa672b026881d634cbc7d01050c
Najmul-Islam <najmulislam519@gmail.com>
2021-04-25 12:02:58 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function App () {
-    return(
-        <div>
-            
-        </div>
-    )
+// #1
+class App extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <Header username="Najmul" />
+                <Greeting />
+            </div>
+        )
+    }
 }
 
+// #2
+class Header extends React.Component{
+    render(){
+        return(
+            <header>
+                <p>Welcome, {this.props.username}</p>
+            </header>
+        )
+    }
+}
+
+// #3
+class Greeting extends Component {
+
+   render(){
+        const date = new Date()
+        const hours = date.getHours()
+        let tiemOfDay
+
+        if (tiemOfDay < 12) {
+            tiemOfDay = "morning"
+        } else if (hours >= 12 && hours < 17) {
+            tiemOfDay = "afternoon"
+        } else {
+            tiemOfDay = "night"
+        }
+
+        return (
+            <h1>Good {tiemOfDay} to you, sir or madam! </h1>
+        )
+   }
+}
 export default App
